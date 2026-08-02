789845c8f911a427b573fdba7a700dada019534c
Your Name <you@example.com>
2026-08-03 00:37:19 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# SPA-React-Portfolio
-Project that showcase React
--- SPA React/src/components/ProjectHeader.jsx
+++
@@ -1,9 +0,0 @@
- function ProjectHeader() {
-  return (
-    <header className="header">
-      <h1>Personal Project Showcase App</h1>
-    </header>
-  );
-}
-
-export default ProjectHeader;
